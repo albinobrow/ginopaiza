ef9806c620d8aab8ea604495b622d91416172d5a
albinobrow <wtasami@hotmail.com>
2020-04-30 15:50:05 +0900

    Create D0154.py

---
+++ D0154.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import sys
+if sys.version_info[0]<3: input=raw_input
+
+class StandardInput:
+
+    def stdin_string(self):
+        self.s=str(input().rstrip())
+        return self.s
+
+    def stdin_integer(self):
+        self.n=int(input().rstrip())
+        return self.n
+
+    def stdin_array(self):
+        self.arr=list(input().rstrip())
+        return self.arr
+
+    def stdin_integer_array(self):
+        self.arr=list(map(int,input().rstrip().split()))
+        return self.arr
+
+def test(arr):
+    return arr[0]**2-arr[1]
+
+if __name__ == '__main__':
+    obj=StandardInput()
+    arr=[]
+    [arr.append(obj.stdin_integer()) for i in range(2)]
+    print(test(arr))
