ea185f6171b2a75601be522fd4d4ba0be05f4385
albinobrow <wtasami@hotmail.com>
2020-12-07 10:30:00 +0900

    Create D0169.py

---
+++ D0169.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import sys
+if sys.version_info[0]<3: input=raw_input
+
+class StandardInput:
+
+    def stdin_string(self):
+        self.s=str(input().rstrip())
+        return self.s
+
+    def stdin_integer(self):
+        self.n=int(input().rstrip())
+        return self.n
+
+    def stdin_array(self):
+        self.arr=input().rstrip().split()
+        return self.arr
+
+    def stdin_integer_array(self):
+        self.arr=list(map(int,input().rstrip().split()))
+        return self.arr
+
+def test(arr):
+    return max(arr)-min(arr)
+
+def main():
+    obj=StandardInput()
+    arr=obj.stdin_integer_array()
+    print(test(arr))
+
+if __name__ == '__main__':
+    main()
