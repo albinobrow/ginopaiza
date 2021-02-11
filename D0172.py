5fbbed4034fc90b4b8e99936cfc37f9222938536
albinobrow <wtasami@hotmail.com>
2021-02-12 08:29:12 +0900

    Create D0172.py

---
+++ D0172.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import sys
+if sys.version_info[0]<3: input=raw_input
+
+class StandardInput:
+
+    def stdin_string(self):
+        self.s=str(input().rstrip())
+        return self.s
+
+    def stdin_integer(self):
+        self.n=int(input().rstrip())
+        return self.n
+
+    def stdin_array(self):
+        self.arr=input().rstrip().split()
+        return self.arr
+
+    def stdin_integer_array(self):
+        self.arr=list(map(int,input().rstrip().split()))
+        return self.arr
+
+def test(n):
+    return n-1
+
+def main():
+    obj=StandardInput()
+    n=obj.stdin_integer()
+    print(test(n))
+
+if __name__ == '__main__':
+    main()
