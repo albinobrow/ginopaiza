3abda9b2eea60a7b193994f604c67317e701b4a5
albinobrow <wtasami@hotmail.com>
2022-02-04 13:22:35 +0900

    Create D0181.py

---
+++ D0181.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import sys
+if sys.version_info[0]<3: input=raw_input
+
+class StandardInput:
+
+    def stdin_string(self):
+        self.s=str(input().rstrip())
+        return self.s
+
+    def stdin_integer(self):
+        self.n=int(input().rstrip())
+        return self.n
+
+    def stdin_array(self):
+        self.arr=input().rstrip().split()
+        return self.arr
+
+    def stdin_integer_array(self):
+        self.arr=list(map(int,input().rstrip().split()))
+        return self.arr
+
+def test(arr):
+    return '-'.join(arr)
+
+def main():
+    obj=StandardInput()
+    arr=[]
+    for i in range(3):
+        arr.append(obj.stdin_string())
+    print(test(arr))
+
+if __name__ == '__main__':
+    main()
