8d44f27da41678080d7647caf048a3bb3c69b1e8
albinobrow <wtasami@hotmail.com>
2022-02-23 21:45:07 +0900

    Create D0186.py

---
+++ D0186.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+import sys
+if sys.version_info[0]<3: input=raw_input
+
+class StandardInput:
+
+    def stdin_string(self):
+        self.s=str(input().rstrip())
+        return self.s
+
+    def stdin_integer(self):
+        self.n=int(input().rstrip())
+        return self.n
+    
+    def stdin_float(self):
+        self.f=float(input().rstrip())
+        return self.f 
+
+    def stdin_array(self):
+        self.arr=input().rstrip().split()
+        return self.arr
+
+    def stdin_integer_array(self):
+        self.arr=list(map(int,input().rstrip().split()))
+        return self.arr
+
+def test(n):
+    if 30 <= n < 35:
+        return "M"
+    else:
+        return n
+
+def main():
+    obj=StandardInput()
+    n=obj.stdin_integer()
+    print(test(n))
+
+if __name__ == '__main__':
+    main()
